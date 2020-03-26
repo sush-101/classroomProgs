d57c0f042be888e6ef01a71510edaf22d0af6a85
sush <59618780+sush-101@users.noreply.github.com>
2020-03-26 21:09:07 +0530

    Update Maximization.java

--- Maximization.java
+++ Maximization.java
@@ -27,23 +27,24 @@ Sample Input:
 Sample Output:
     114*/
     import java.util.*;
-class Maximization{
-    static String dp[][];
+public class Main{
+    static char dp[][];
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), arr[][] = new int[n][n],max = Integer.MIN_VALUE;
-        dp = new String[n][n];
-        for(String[] c:dp)
-        Arrays.fill(c,"#");
+        dp = new char[n][n];
+        for(char[] c:dp)
+        Arrays.fill(c,'#');
         for(int i=0;i<n;i++)for(int j=0;j<n;j++) arr[i][j] = sc.nextInt();
         for(int i = 0;i < n ;i++) max = Math.max(max,find(arr,n-1,i));
         System.out.println(max);
     }
     static int find(int arr[][],int row,int col){
         if(row<0 || col>=arr.length || col<0) return 0;
-        if(!dp[row][col].equals("#")) return Integer.parseInt(dp[row][col]);
+        if(dp[row][col]!='#')return (int)dp[row][col];
         int ans = arr[row][col];
-        dp[row][col] = String.valueOf((ans + Math.max(find(arr,row-1,col),Math.max(find(arr,row-1,col-1),find(arr,row-1,col+1)))));
-        return Integer.parseInt(dp[row][col]);
+        ans += Math.max(find(arr,row-1,col),Math.max(find(arr,row-1,col-1),find(arr,row-1,col+1)));
+        dp[row][col] = (char)ans;
+        return (int)dp[row][col];
     }
 }
